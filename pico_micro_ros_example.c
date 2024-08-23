5aba8ae2d1851027e7c93b351c732497f2ee2840
craiglawtondevine <craig.lawtondevine@gmail.com>
2024-08-23 12:38:06 +0100

    Modifications for Pico W board

--- pico_micro_ros_example.c
+++ pico_micro_ros_example.c
@@ -8,6 +8,7 @@
 #include <rmw_microros/rmw_microros.h>
 
 #include "pico/stdlib.h"
+#include "pico/cyw43_arch.h"
 #include "pico_uart_transports.h"
 
 const uint LED_PIN = 25;
@@ -19,21 +20,31 @@ void timer_callback(rcl_timer_t *timer, int64_t last_call_time)
 {
     rcl_ret_t ret = rcl_publish(&publisher, &msg, NULL);
     msg.data++;
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, msg.data % 2);
+    // printf("callback was hit. Value: %u", msg.data);
 }
 
 int main()
 {
-    rmw_uros_set_custom_transport(
-		true,
-		NULL,
-		pico_serial_transport_open,
-		pico_serial_transport_close,
-		pico_serial_transport_write,
-		pico_serial_transport_read
-	);
+    const int flash_delay_ms = 250;
+    const int flash_fast_delay_ms = 100;
+    const int flash_slow_delay_ms = 500;
+    // printf("Booting");
 
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
+    stdio_init_all();
+    if (cyw43_arch_init())
+    {
+        // printf("Wi-Fi init failed");
+        return -1;
+    }
+
+    rmw_uros_set_custom_transport(
+        true,
+        NULL,
+        pico_serial_transport_open,
+        pico_serial_transport_close,
+        pico_serial_transport_write,
+        pico_serial_transport_read);
 
     rcl_timer_t timer;
     rcl_node_t node;
@@ -43,42 +54,60 @@ int main()
 
     allocator = rcl_get_default_allocator();
 
-    // Wait for agent successful ping for 2 minutes.
-    const int timeout_ms = 1000; 
-    const uint8_t attempts = 120;
+    // Wait for agent successful ping
+    const int timeout_ms = 1000;
+    const uint8_t attempts = 5;
 
-    rcl_ret_t ret = rmw_uros_ping_agent(timeout_ms, attempts);
-
-    if (ret != RCL_RET_OK)
+    // show that we woke up/alive
+    for (int i = 0; i < 3; i++)
     {
-        // Unreachable agent, exiting program.
-        return ret;
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+        sleep_ms(flash_fast_delay_ms);
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+        sleep_ms(flash_fast_delay_ms);
     }
 
-    rclc_support_init(&support, 0, NULL, &allocator);
-
-    rclc_node_init_default(&node, "pico_node", "", &support);
-    rclc_publisher_init_default(
-        &publisher,
-        &node,
-        ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
-        "pico_publisher");
-
-    rclc_timer_init_default(
-        &timer,
-        &support,
-        RCL_MS_TO_NS(1000),
-        timer_callback);
-
-    rclc_executor_init(&executor, &support.context, 1, &allocator);
-    rclc_executor_add_timer(&executor, &timer);
-
-    gpio_put(LED_PIN, 1);
-
-    msg.data = 0;
     while (true)
     {
-        rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100));
+        // printf("try to connect to the agent");
+        rcl_ret_t ret = rmw_uros_ping_agent(timeout_ms, attempts);
+        if (ret == RCL_RET_OK)
+            break;
+
+        for (int i = 0; i < 3; i++)
+        {
+            // Unreachable agent, flash fast
+            cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+            sleep_ms(flash_fast_delay_ms);
+            cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+            sleep_ms(flash_fast_delay_ms);
+        }
     }
+
+        msg.data = 0;
+
+        rclc_support_init(&support, 0, NULL, &allocator);
+
+        rclc_node_init_default(&node, "pico_node", "", &support);
+        rclc_publisher_init_default(
+            &publisher,
+            &node,
+            ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
+            "pico_publisher");
+
+        rclc_timer_init_default(
+            &timer,
+            &support,
+            RCL_MS_TO_NS(1000),
+            timer_callback);
+
+        rclc_executor_init(&executor, &support.context, 1, &allocator);
+        rclc_executor_add_timer(&executor, &timer);
+
+        while (true)
+        {
+            rclc_executor_spin_some(&executor, RCL_MS_TO_NS(flash_slow_delay_ms));
+        }
+
     return 0;
 }
